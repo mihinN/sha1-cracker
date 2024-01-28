3c72bbe0e44acbad17972a21c2cc0bae0df031b3
mihinN <anonnimnaka@gmail.com>
2024-01-28 23:12:20 +0530

    env

--- sha1/src/main.rs
+++ sha1/src/main.rs
@@ -1,8 +1,23 @@
+/********************************
+ * PROJECT : SIMPLE SHA1 CRACKER 
+ * DATE : 28TH JANUARY 2023
+ ********************************
+ ******************************** 
+*/
+
 use std::io;
 use std::env;
 
 fn main() {
-    print!("Cracker - sha1 - simple simple \n");
+
+    print!("Sha1-Decryptor\n");
+    println!("===============");
+
     let args_buffer : Vec<String> = env::args().collect();
-    dbg!(args_buffer);
+    // dbg!(args_buffer);
+        if args_buffer.len() != 3 {
+            print!("Usage \n");
+            print!("Decryptor - : <word list> <sha_hash> \n");
+            return;
+        }
 }
